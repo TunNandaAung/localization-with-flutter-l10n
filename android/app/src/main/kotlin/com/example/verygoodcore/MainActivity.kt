68449ea9ff2d8cc73e27632cfe0c656ed2194084
TunNandaAung <tunnandaaung@outlook.com>
2022-07-05 19:32:59 +0630

    Init

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.l10n_with_flutter_l10n
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_development.dart
@@ -0,0 +1,13 @@
+// Copyright (c) 2022, Very Good Ventures
+// https://verygood.ventures
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
+import 'package:l10n_with_flutter_l10n/app/app.dart';
+import 'package:l10n_with_flutter_l10n/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
